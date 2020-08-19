f4ddaea94e6979dfb04259e674f13d58485141cd
Randy Steele <55413005+steelerx155@users.noreply.github.com>
2020-08-19 16:55:53 -0400

    Update CommentInput.js

--- gone-fishin-frontend/src/components/CommentInput.js
+++ gone-fishin-frontend/src/components/CommentInput.js
@@ -9,11 +9,12 @@ class CommentInput extends React.Component {
     state = {
         content: ""
     }
+//this is setting local state
 
     handleChange = (event) => {
         this.setState({
             [event.target.name]: event.target.value            
-        })
+        }) //this handle change function let's us know when a user has started typing in our form. 
     }
 
     handleSubmit = (event) => {
@@ -21,7 +22,7 @@ class CommentInput extends React.Component {
         this.props.addComment(this.state, this.props.destinations.id)    
         this.setState({
          content: ''
-        })
+        }) // handle submit handles the subitting of the forms and updates our state. 
     }
     
 
@@ -30,9 +31,10 @@ class CommentInput extends React.Component {
             <div>
                 <form onSubmit={this.handleSubmit}>
                     <label>Comment:</label>
-                    <input type="text" name="content" value ={this.state.content} onChange={this.handleChange}/>
+                    <input type="text" name="content" value ={this.state.content} onChange={this.handleChange}/> 
+            // this is call the onchange on the content text field so we know when the user has started typing. 
                     <Button type="submit">Submit</Button>
-
+            // sumit button 
                 </form>
 
 
