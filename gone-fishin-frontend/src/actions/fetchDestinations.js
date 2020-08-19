da41078b2261a852cfe53f372c14948454feac8f
Randy Steele <55413005+steelerx155@users.noreply.github.com>
2020-08-19 16:50:16 -0400

    Update fetchDestinations.js

--- gone-fishin-frontend/src/actions/fetchDestinations.js
+++ gone-fishin-frontend/src/actions/fetchDestinations.js
@@ -2,12 +2,14 @@
 
 export function fetchDestinations() {
   return (dispatch) => {
+    //fetch to the api to get all destinations
    fetch("http://localhost:3000/api/v1/destinations/")
     .then(response => response.json())
     .then(destinations => dispatch({
       type: 'FETCH_DESTINATIONS',
        payload: destinations
     }))
+    //dispatches the fetch destinations action to the store
   }
 }
 
