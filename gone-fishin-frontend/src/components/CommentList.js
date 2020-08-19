ebb7f8f551ce4a09fb4a3da54141e32a3cbcb4e8
Randy Steele <55413005+steelerx155@users.noreply.github.com>
2020-08-19 16:59:59 -0400

    Update CommentList.js

--- gone-fishin-frontend/src/components/CommentList.js
+++ gone-fishin-frontend/src/components/CommentList.js
@@ -12,6 +12,7 @@ const CommentList = (props) => {
            {props.comments && props.comments.map(comment =>
             <li key={comment.id}>{comment.content}<br></br>
         //if there are comments we want to get the content of the comments
+        //we have a key prop as a unique identifier.
             </li>
             )} 
         </div>
@@ -19,3 +20,4 @@ const CommentList = (props) => {
 }
 
 export default connect(null, {addComment}) (CommentList)
+// we don't need mapstatetoprops so we call null, addComment is equivelant to mapDispatchtoprops
