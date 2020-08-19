5f7a5a8e734f12dab4306e4d7c33e3c9c61a3076
Randy Steele <55413005+steelerx155@users.noreply.github.com>
2020-08-19 16:57:22 -0400

    Update CommentList.js

--- gone-fishin-frontend/src/components/CommentList.js
+++ gone-fishin-frontend/src/components/CommentList.js
@@ -5,12 +5,13 @@ import {addComment} from '../actions/addComment'
 
 
 const CommentList = (props) => {
-
+// passing props to our stateless commentList functional component. 
 
     return (
         <div>
            {props.comments && props.comments.map(comment =>
             <li key={comment.id}>{comment.content}<br></br>
+        //if there are comments we want to get the content of the comments
             </li>
             )} 
         </div>
