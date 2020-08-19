5a593dec9d2934138de9156104cf29bba1c15811
Randy Steele <55413005+steelerx155@users.noreply.github.com>
2020-08-19 15:47:47 -0400

    Update deleteFeature.js

--- gone-fishin-frontend/src/actions/deleteFeature.js
+++ gone-fishin-frontend/src/actions/deleteFeature.js
@@ -1,10 +1,10 @@
 export const deleteFeature = (featureId, destinationId) => {
-
+  // fetch to the api to a particular feature of a particular destination. 
     return (dispatch) => {
        return fetch(`http://localhost:3000/api/v1/destinations/${destinationId}/features/${featureId}`, { 
         method: 'DELETE'       
     })
      .then(response => response.json())
      .then(destination => dispatch({type: 'DELETE_FEATURE', payload: destination}))
-  }
+  } // dispatches the delete feature action to the store. 
 }
