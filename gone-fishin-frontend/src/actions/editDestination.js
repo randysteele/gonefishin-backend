fc86e3b6f4cccae0f1e59fb4b0abe218985d7584
Randy Steele <55413005+steelerx155@users.noreply.github.com>
2020-08-19 15:55:56 -0400

    Update editDestination.js

--- gone-fishin-frontend/src/actions/editDestination.js
+++ gone-fishin-frontend/src/actions/editDestination.js
@@ -1,5 +1,5 @@
 export const editDestination = (data) => {
-    
+    // fetches to the api to get a particular destination
     return (dispatch) => {
         fetch(`http://localhost:3000/api/v1/destinations/${data.id}`, {
             headers: {
@@ -12,4 +12,5 @@ export const editDestination = (data) => {
         .then(response => response.json())
         .then(destination => dispatch({type: 'EDIT_DESTINATION', payload: destination}))
     }
+    //dispatches the edit destination actions to the store
 }
