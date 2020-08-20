705d010f0eaf0b6bbf28a9e7fb44cfcfcfef7801
Randy Steele <55413005+steelerx155@users.noreply.github.com>
2020-08-20 19:02:46 -0400

    Update App.js

--- gone-fishin-frontend/src/App.js
+++ gone-fishin-frontend/src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import DestinationsContainer from './containers/DestinationsContainer'
 import styled from 'styled-components'
 import Navbar from './components/Navbar'
+// we want the Navbar to be used within our whole app so it needs to be imported in app.js
 
 const Title = styled.h1`
 color: blue;
 font-size: 100px;
 `
+// setting a default style for our title which is called below.
 
 
 
@@ -19,8 +21,10 @@ class App extends React.Component {
       <div className="App">
         <Navbar />
         <Title>Gone Fishin'</Title>
+      // here we render App, Navbar and Title (as mentioned above)
 
       <DestinationsContainer />  
+      // rendering the destinationscontainer
 
 
 
