f26bce21fcc04ebcd6ee757745c2e887f9bbc623
Randy Steele <55413005+steelerx155@users.noreply.github.com>
2020-08-20 18:36:43 -0400

    Update Navbar.js

--- gone-fishin-frontend/src/components/Navbar.js
+++ gone-fishin-frontend/src/components/Navbar.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+// we need access to this so we can use Link tags. 
 
 const Navbar = (props) => {
+    // creating the component and passing it props. 
 
     return (
         <div>
             
             <Link to='/destinations' style={{paddingRight: '10px'}}>Destinations    </Link>
             <Link to='/destinations/new'>Add Destination    </Link>
-
+            // linking to these routes (all destinations and add destinations. 
             
         </div>
     )
