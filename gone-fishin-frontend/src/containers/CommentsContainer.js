3b312c18352e7fe08aecd6d4afeb328f24a42fd8
Randy Steele <55413005+steelerx155@users.noreply.github.com>
2020-08-20 18:38:45 -0400

    Update CommentsContainer.js

--- gone-fishin-frontend/src/containers/CommentsContainer.js
+++ gone-fishin-frontend/src/containers/CommentsContainer.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import CommentInput from '../components/CommentInput'
 import CommentList from '../components/CommentList'
+// importing the files that are related to the commentsContainer
 
 
 
 class CommentsContainer extends React.Component {
+    // class component 
     
 
 
@@ -12,7 +14,9 @@ class CommentsContainer extends React.Component {
         return (
             <div>
                 <CommentInput destinations={this.props.destination}/><br></br>
+            // rendering the commentInput component 
                 <CommentList comments={this.props.destination && this.props.destination.comments}/>
+            // rendering the commentlist component and if that destination has comments that belong to it we want to show them. 
             </div>
 
         )
