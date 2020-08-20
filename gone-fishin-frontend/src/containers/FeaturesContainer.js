d7244413932ce29e1e782a0151dc11b595ca6ee9
Randy Steele <55413005+steelerx155@users.noreply.github.com>
2020-08-20 18:56:37 -0400

    Update FeaturesContainer.js

--- gone-fishin-frontend/src/containers/FeaturesContainer.js
+++ gone-fishin-frontend/src/containers/FeaturesContainer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import FeaturesInput from '../components/FeaturesInput'
 import FeaturesList from '../components/FeaturesList'
+// importing the files we need access to, the ones that are related to features.
 
 
 
@@ -12,7 +13,9 @@ class FeaturesContainer extends React.Component {
         return (
             <div>
                 <FeaturesInput destinations={this.props.destination}/><br></br>
+            // rendering the featuresInput component
                 <FeaturesList features={this.props.destination && this.props.destination.features}/>
+            // rendering the featuresList component and rendering the features of particular destinations. 
             </div>
         )
     }
