94abc01b1e58a888995be252362cb61c2e7e9850
Randy Steele <55413005+steelerx155@users.noreply.github.com>
2020-08-20 18:54:00 -0400

    Update DestinationsContainer.js

--- gone-fishin-frontend/src/containers/DestinationsContainer.js
+++ gone-fishin-frontend/src/containers/DestinationsContainer.js
@@ -4,7 +4,9 @@ import {fetchDestinations} from '../actions/fetchDestinations'
 import DestinationsList from  '../components/DestinationsList'
 import DestinationsInput from '../components/DestinationsInput'
 import DestinationShow from  '../components/DestinationShow'
+// we need access to these destination related components so we can render them
 import {Route, Switch} from 'react-router-dom'
+// we need access to Route and Switch so we can use them in this container component
 
 
 
@@ -15,6 +17,7 @@ class DestinationsContainer extends React.Component {
     componentDidMount(){
       this.props.fetchDestinations()
     }
+    // we are placing our initial fetch call in our component did mount function. 
 
     render(){        
       return (
@@ -24,6 +27,7 @@ class DestinationsContainer extends React.Component {
          <Route  path='/destinations/:id'  render={(routerProps) => <DestinationShow {...routerProps} destinations={this.props.destinations}/>}/>   
          <Route  path='/destinations' render={(routerProps) => <DestinationsList {...routerProps} destinations={this.props.destinations}/>}/>
          </Switch><br></br>
+             // Setting up 3 routes destinations/new destinations/:id and all destinations(akd show)
          
         </div>
       )
@@ -34,5 +38,7 @@ const mapStateToProps = state => {
     return {
         destinations: state.destinations
     } 
+    // here we are telling connect that we want access to this piece of data from the store. 
 }
 export default connect(mapStateToProps, {fetchDestinations})(DestinationsContainer);
+// connecting to the store and passing mapStateToProps as well as fetchDestinations. 
