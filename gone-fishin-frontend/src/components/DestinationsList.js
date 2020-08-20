4cc571aed6d8ab08d709a39f07cd1f73895e49b7
Randy Steele <55413005+steelerx155@users.noreply.github.com>
2020-08-20 18:27:12 -0400

    Update DestinationsList.js

--- gone-fishin-frontend/src/components/DestinationsList.js
+++ gone-fishin-frontend/src/components/DestinationsList.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+// import this so we can use Link tags
 
 
 
 const DestinationsList = (props) => {
+    //creating the class and passing props
     
     return (
        <div>
        <h2> Fishing Lake List:</h2> {props.destinations.map(dest => <h5><div key={dest.id}>Lake Name:<Link to={`/destinations/${dest.id}`}><br></br>{dest.name}</Link>  <br></br> City: {dest.city} <br></br> State: {dest.state} <br></br> Description: {dest.description}</div></h5>)} 
-   </div>
-   
+   </div> 
+// mapping over props.destinations so that we can get access to the city, state, name and description. we use a key prop so that there is a unique identifier
+// this is a simple class and there isn't a whole lot happening here.    
  
     )
 }
